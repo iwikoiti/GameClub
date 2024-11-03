ee6276551cfa9f03981ea1ffa4c3e48d3844a7db
yui <yuineor@gmail.com>
2024-11-03 19:21:36 +0700

    Добавьте файлы проекта.

---
+++ GameClub/GCLogIn.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace GameClub
+{
+    partial class GCLogIn
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.authentificationTableAdapter1 = new GameClub.GamuClubDBDataSetTableAdapters.AuthentificationTableAdapter();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // loginInput
+            // 
+            this.loginInput.Location = new System.Drawing.Point(82, 89);
+            this.loginInput.Name = "loginInput";
+            this.loginInput.Size = new System.Drawing.Size(220, 27);
+            this.loginInput.TabIndex = 2;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(82, 166);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.PasswordChar = '*';
+            this.passwordInput.Size = new System.Drawing.Size(220, 27);
+            this.passwordInput.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.loginBtn);
+            this.groupBox1.Controls.Add(this.loginInput);
+            this.groupBox1.Controls.Add(this.passwordInput);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(236, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 285);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вход";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(144, 226);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(89, 32);
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.Text = "Войти";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // authentificationTableAdapter1
+            // 
+            this.authentificationTableAdapter1.ClearBeforeFill = true;
+            // 
+            // GCLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 555);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GCLogIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GameClub Вход";
+            this.Load += new System.EventHandler(this.GCLogIn_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox loginInput;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button loginBtn;
+        private GamuClubDBDataSetTableAdapters.AuthentificationTableAdapter authentificationTableAdapter1;
+    }
+}
+
---
+++ GameClub/GCLogIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameClub
+{
+    public partial class GCLogIn : Form
+    {
+        public GCLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void GCLogIn_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+            string login = (loginInput.Text).ToString();
+            string password = (passwordInput.Text).ToString();
+
+            var role = (authentificationTableAdapter1.GetDataBy(login, password)).ToString();
+
+            if (role == "ADMIN")
+            {
+                GCAdmin gcAdmin = new GCAdmin();
+                gcAdmin.Show();
+                //this.Hide();
+                //this.Show();
+            }
+            else
+            {
+                GCUser gcUser = new GCUser();
+                gcUser.Show();
+                //this.Hide();
+                //this.Show();
+
+            }
+        }
+
+    }
+}
